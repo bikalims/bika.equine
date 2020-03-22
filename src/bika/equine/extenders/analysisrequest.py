aeecfdf9c839eb300af60ecf4409f3b983a56779
Lunga Baliwe <lunga001@gmail.com>
2020-03-22 10:38:02 +0200

    Change label

--- src/bika/equine/extenders/analysisrequest.py
+++ src/bika/equine/extenders/analysisrequest.py
@@ -1,4 +1,5 @@
 from archetypes.schemaextender.interfaces import ISchemaModifier
+from bika.lims import bikaMessageFactory as _
 from bika.lims.interfaces import IAnalysisRequest
 from zope.component import adapts
 from zope.interface import implements
@@ -15,4 +16,6 @@ class AnalysisRequestSchemaModifier(object):
         """
         """
         schema['SamplingDeviation'].required = True
+        schema['ClientReference'].widget.label = _('Seal Number')
+        schema['ResultsInterpretation'].widget.label = _('Comment')
         return schema
